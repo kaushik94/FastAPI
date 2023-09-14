ee19bb118379d3da02170826226236d29d125f70
Kaushik Varanasi <kaushik@Kaushiks-MacBook-Pro.local>
2023-09-14 05:30:35 +0530

    add sidebar route

--- main.py
+++ main.py
@@ -5,3 +5,7 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"greeting": "Hello, World!", "message": "Welcome to FastAPI!"}
+
+@app.get('/sidebar')
+def send_iframe_html():
+    return '<p>App content from the server</p>'
